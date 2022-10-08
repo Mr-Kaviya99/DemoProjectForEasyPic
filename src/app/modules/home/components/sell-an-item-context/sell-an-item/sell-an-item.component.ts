5cc69f73156eb5c3faa0fa2dfd2d7d8a9038bb4a
Mr-Kaviya99 <kavinduhasintha9@gmail.com>
2022-10-08 09:48:13 +0530

    fixed route

--- src/app/modules/home/components/sell-an-item-context/sell-an-item/sell-an-item.component.ts
+++ src/app/modules/home/components/sell-an-item-context/sell-an-item/sell-an-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-sell-an-item',
@@ -7,14 +8,59 @@ import {Router} from "@angular/router";
   styleUrls: ['./sell-an-item.component.scss']
 })
 export class SellAnItemComponent implements OnInit {
+  vehicles: boolean = false;
+  electronics: boolean = false;
+  property: boolean = false;
+  service: boolean = false;
+  animals: boolean = false;
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    public dialogRef: MatDialogRef<any>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {
+  }
 
   ngOnInit(): void {
   }
-  navigateToRoute(link: string) {
-    this.router.navigate(['/home/sell-an-item/' + link])
+
+  setElectronicVisible() {
+    this.vehicles = false;
+    this.electronics =true;
+    this.property= false;
+    this.service=false;
+    this.animals=false;
+  }
+
+  setVehiclesVisible() {
+    this.vehicles = true;
+    this.electronics =false;
+    this.property= false;
+    this.service=false;
+    this.animals=false;
+  }
+
+  setPropertyVisible() {
+    this.vehicles = false;
+    this.electronics =false;
+    this.property= true;
+    this.service=false;
+    this.animals=false;
+  }
+
+  setServiceVisible() {
+    this.vehicles = false;
+    this.electronics =false;
+    this.property= false;
+    this.service=true;
+    this.animals=false;
+  }
+
+  setAnimalsVisible() {
+    this.vehicles = false;
+    this.electronics =false;
+    this.property= false;
+    this.service=false;
+    this.animals=true;
   }
 }
